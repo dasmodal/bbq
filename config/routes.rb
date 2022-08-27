c63bacf881a0e1397704b58759437b75181151ee
Artur Rodkin <das.modal@gmail.com>
2022-08-27 11:34:28 +0700

    feature/ add comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: "events#index"
   devise_for :users
 
+  resources :comments do
+    resources :comments, only: %i[create destroy]
+  end
   resources :events
   resources :users, only: %i[show edit update]
 end
