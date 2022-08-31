6ffd8e638d35617e1ce325724397ceca1ff39fff
Artur Rodkin <das.modal@gmail.com>
2022-08-31 11:17:02 +0700

    add helpers for display avatar and event backgrounds photo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,32 +1,45 @@
 module ApplicationHelper
   def user_avatar(user)
-<<<<<<< HEAD
-    if user.avatar?
-      user.avatar.url
+    if user.avatar.attached?
+      user.avatar.variant(resize_to_fit: [200, 200])
     else
       asset_pack_path 'media/images/user.png'
     end
   end
 
   def user_avatar_thumb(user)
-    if user.avatar.file.present?
-      user.avatar.thumb.url
+    if user.avatar.attached?
+      user.avatar.variant(resize_to_fit: [50, 50])
     else
       asset_pack_path 'media/images/user.png'
     end
   end
 
-  def event_photo(event)
-    photos = event.photos.persisted
-
-    if photos.any?
-      photos.sample.photo.url
+  def bg_event_photo(event)
+    if event.bg_photo.attached?
+      event.bg_photo
     else
       asset_pack_path 'media/images/event.jpg'
     end
-=======
-    # TODO
-    asset_pack_path 'media/images/user.png'
->>>>>>> parent of ac55893... feature/ users can upload avatar and event photos
+  end
+
+  def bg_event_photo_thumb(event)
+    if event.bg_photo.attached?
+      event.bg_photo.variant(resize_to_fit: [600, 600])
+    else
+      asset_pack_path 'media/images/event.jpg'
+    end
+  end
+
+  def sample_event_photo(event)
+    # TODO / for event photo, choice random if many photos
+
+    # photos = event.photos.persisted
+
+    # if photos.any?
+    #   photos.sample.photo.url
+    # else
+    #   asset_pack_path 'media/images/event.jpg'
+    # end
   end
 end
