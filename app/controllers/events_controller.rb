e9040635188ec4e1862765a5817d976defac68f0
Artur Rodkin <das.modal@gmail.com>
2022-09-08 00:30:50 +0700

    add pincode to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: %i[show index]
   before_action :set_event, only: %i[show]
   before_action :set_current_user_event, only: %i[edit update destroy]
+  before_action :password_quard!, only: %i[show]
 
   def index
     @events = Event.all
@@ -51,6 +52,20 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:title, :address, :datetime, :description, :bg_photo, photos: [])
+    params.require(:event).permit(:title, :address, :datetime, :description, :pincode, :bg_photo, photos: [])
+  end
+
+  def password_quard!
+    return true if @event.pincode.blank?
+    return true if signed_in? && current_user = @event.user
+
+    if params[:pincode].present? && @event.pincode_valid?(params[:pincode])
+      cookies.permanent["events_#{@event.id}_pincode"] = params[:pincode]
+    end
+
+    unless @event.pincode_valid?(cookies.permanent["events_#{@event.id}_pincode"])
+      flash.now[:alert] = I18n.t('controllers.events.wrong_pincode') if params[:pincode].present?
+      render 'password_form'
+    end
   end
 end
