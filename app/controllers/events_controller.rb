be6b06811184e21636e45bca88466923b6822169
Artur Rodkin <das.modal@gmail.com>
2022-09-25 18:06:16 +0700

    [69-1]fix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: %i[show index]
-  before_action :set_event, only: %i[show delete_bg_image]
+  before_action :set_event, only: %i[show]
   before_action :set_current_user_event, only: %i[edit update destroy delete_bg_image]
   before_action :password_quard!, only: %i[show]
 
