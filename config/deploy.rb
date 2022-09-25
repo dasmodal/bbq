9fa792f6938495f5d42584340e4e949a655702bb
Artur Rodkin <das.modal@gmail.com>
2022-09-25 09:40:01 +0700

    try new capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,13 @@
-# config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
 set :application, "grillmeal"
 set :repo_url, "git@github.com:dasmodal/bbq.git"
-set :deploy_to, "/home/deploy/www/grillmeal"
-
-set :ssh_options, {
-  forward_agent: true,
-  port: 3456
-}
+set :branch, 'add_capistrano'
+set :deploy_to, "/home/deploy/www/#{fetch :application}"
 
 set :log_level, :info
 
-append :linked_files, "config/database.yml", "config/master.key"
+append :linked_files, "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
 
 namespace :deploy do
