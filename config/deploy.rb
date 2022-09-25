2680b055121b8e853e2a2c3a3c61094e62767684
Artur Rodkin <das.modal@gmail.com>
2022-09-25 15:13:16 +0700

    try to fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,16 +9,3 @@ set :log_level, :info
 
 append :linked_files, "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-
-namespace :deploy do
-
-  desc "Restart application"
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join("tmp/restart.txt")
-    end
-  end
-
-  after :finishing, "deploy:cleanup"
-
-end
