af3ab7e4886ed14b75be140d2e52265675abef03
Artur Rodkin <das.modal@gmail.com>
2022-11-20 13:21:42 +0700

    config letter_opener for run only dev env

--- config/initializers/letter_opener.rb
+++ config/initializers/letter_opener.rb
@@ -1,7 +1,9 @@
-LetterOpener.configure do |config|
-  # To change default file URI scheme you can provide `file_uri_scheme` config.
-  # It might be useful when you use WSL (Windows Subsystem for Linux) and default
-  # scheme doesn't work for you.
-  # Default value is blank
-  config.file_uri_scheme = 'file://wsl.localhost/Ubuntu/'
+if Rails.env.development?
+  LetterOpener.configure do |config|
+    # To change default file URI scheme you can provide `file_uri_scheme` config.
+    # It might be useful when you use WSL (Windows Subsystem for Linux) and default
+    # scheme doesn't work for you.
+    # Default value is blank
+    config.file_uri_scheme = 'file://wsl.localhost/Ubuntu/'
+  end
 end
