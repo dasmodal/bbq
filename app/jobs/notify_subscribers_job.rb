107896903a451d66328ac0c0ec1224deb6d42c7e
Artur Rodkin <das.modal@gmail.com>
2022-11-26 23:34:07 +0700

    refactoring NotifySubscribersJob, correct mailer for send all email together

--- app/jobs/notify_subscribers_job.rb
+++ app/jobs/notify_subscribers_job.rb
@@ -6,8 +6,6 @@ class NotifySubscribersJob < ApplicationJob
     model_type = model.class.name.downcase
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [model.user&.email]).uniq
 
-    all_emails.each do |mail|
-      EventMailer.send(model_type, model, mail).deliver_now
-    end
+    EventMailer.send(model_type, model, all_emails).deliver_now
   end
 end
